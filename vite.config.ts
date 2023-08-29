01bd7055497961b76b5352fb140883890558426f
Rajat Verma <rajat311201@gmail.com>
2023-08-29 09:57:30 +0530

    added react-query, react router

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// Don't forget to `npm i -D @types/node`, so __dirname won't complain
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    },
+});
